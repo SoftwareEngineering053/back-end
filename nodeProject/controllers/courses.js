3b3964da71593a1607843acfd002204645cefd8a
ivan20022002 <103858300+ivan20022002@users.noreply.github.com>
2023-01-29 13:20:51 +0100

    Update courses.js

--- nodeProject/controllers/courses.js
+++ nodeProject/controllers/courses.js
@@ -4,11 +4,21 @@ const newCourse = (req, res, next) => {
     //res.json({message: "new course posted"});
 };
 
-const allCourses = (req, res, next) => {
-    //res.json({message: "new tea posted"});
-    res.json({title: "this is a course"});
-};
 
+const allCourses  = async (req, res, next) => {
+    //res.json({message: "new tea posted"});
+    let courses = await model.find({});
+    courses = courses.map((course) => {
+        return {
+            id: course.id,
+            titolo: course.titolo,
+            abstract: course.abstract,
+            docente: course.docente,
+            studentiIscritti: course.studentiIscritti
+        }
+    });
+    res.status(200).json(courses);
+};
 //const addCourse
 
 //const addSubscriber

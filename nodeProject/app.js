45bff40821f37ee023bbcde54c7d5dd6caa4e930
ivan20022002 <103858300+ivan20022002@users.noreply.github.com>
2023-02-01 11:02:45 +0100

    Update app.js

--- nodeProject/app.js
+++ nodeProject/app.js
@@ -1,16 +1,16 @@
 const dotenv = require('dotenv').config();
 const express = require('express');
 const app = express();
-const swaggerUi=require('swagger-ui-express');
-const swaggerDocument=require('../swagger.json');
+/*const swaggerUi=require('swagger-ui-express');
+const swaggerDocument=require('../swagger.json');*/
 
 console.log(process.env.DB_HOST);
 
-app.use(
+/*app.use(
     '/api-docs',
     swaggerUi.serve,
     swaggerUi.setup(swaggerDocument)
-);
+);*/
 
 app.get("/", function (req, res) {
     res.send(req.headers, req.originalUrl, req.method, req.body);
